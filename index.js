875038902a23c06a9703fa82d48ef00f836c7ce5
dominick <dominickugas@gmail.com>
2021-10-14 20:38:19 -0400

    commit tree d82a7ebda16e27c43f9fc76e9246da1b79de95a6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const newArray = [
+    { year: 1966, result: "L" },
+    { year: 1969, result: "W" },
+    { year: 1970, result: "not available" },
+]
+
+function superbowlWin(newArray) {
+    let theWin = newArray.find(object => object.result === "W")
+    if (theWin === undefined) {
+        return undefined
+    } else {
+        return theWin.year
+    }
+
+}
+superbowlWin(newArray)
+
